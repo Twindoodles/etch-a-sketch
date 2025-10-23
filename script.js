7daf5a42b888c9d51bc2e3441c2a603caf75da3c
twindoodles <47507866+Twindoodles@users.noreply.github.com>
2025-10-24 00:54:48 +0800

    create reset canvas button

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const grid = document.querySelector("#grid");
 
-function createGrid(a,b) {
-    for (i = 0; i < a; i++) {
+function createGrid(num) {
+    for (i = 0; i < num; i++) {
         var column = document.createElement("div");
         column.id = "column";
-        for (j = 0; j < b; j++) {
+        for (j = 0; j < num; j++) {
             var square = document.createElement("div");
             square.id = "square";
             column.appendChild(square);
@@ -19,4 +19,13 @@ document.addEventListener("mouseover", e => {
     }
 });
 
-createGrid(16,16);
+const button = document.querySelector("button");
+button.addEventListener("click", e => {
+    let dimensions = prompt("What res do you want the next canvas? (Enter the number of pixels of one side)");
+    while (grid.children.length > 0) {
+        grid.removeChild(grid.lastChild);
+    }
+    createGrid(dimensions);
+});
+
+createGrid(16);
